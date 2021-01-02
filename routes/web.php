054ccb0daafb012dacaffff23d7b4a7d325c11c2
YamaKasa3 <yt.const@gmail.com>
2021-01-02 23:16:58 +0900

    Post

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,17 @@ Route::get('/', function () {
         'name' => $name,
     ]);
 });
+
+Route::get('/posts/{post}', function($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first post!',
+        'my-second-post' => 'Second post',
+    ];
+
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found.');
+    }
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
+});
