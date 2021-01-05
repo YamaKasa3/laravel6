46aa3373f60e3dc6d1dd66ba317e6716ce28127c
YamaKasa3 <yt.const@gmail.com>
2021-01-06 01:30:42 +0900

    Render Dynamical Data

--- database/migrations/2021_01_05_154920_create_articles_table.php
+++ database/migrations/2021_01_05_154920_create_articles_table.php
@@ -15,6 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('except');
+            $table->text('body');
             $table->timestamps();
         });
     }
