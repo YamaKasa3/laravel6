e45f671825b9e53ce88b3a4ca7df08110ef5b6db
YamaKasa3 <yt.const@gmail.com>
2021-01-08 23:52:00 +0900

    Basic Eloquent Relationships

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class); // select * from articles where user_id
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
