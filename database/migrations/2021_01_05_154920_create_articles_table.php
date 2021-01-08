ed6078659898623e89884a60e3f7a135628c6501
YamaKasa3 <yt.const@gmail.com>
2021-01-09 01:01:34 +0900

    Understanding Foreign Keys and Database Factories

--- database/migrations/2021_01_05_154920_create_articles_table.php
+++ database/migrations/2021_01_05_154920_create_articles_table.php
@@ -15,11 +15,19 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->text('except');
             $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
+
+        
     }
 
     /**
